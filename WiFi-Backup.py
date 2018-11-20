24d6ecb7c1dc06c67f4d23391f35f320ee9a9bfa
Seinu <seinu.fireflower@gmail.com>
2018-11-20 18:52:06 -0500

    Update WiFi-Backup.py

--- WiFi-Backup.py
+++ WiFi-Backup.py
@@ -14,16 +14,16 @@
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #*****************************************************************************'
-#	File Name:	getWifiKeys.vbs, WiFi-Backup.py
-#	Authors:	Hackoo & Seinu
-#	Purpose:	Backup WiFi Keys from MacOS, Windows and Linux 
-#	History:
+#   File Name:	getWifiKeys.vbs, WiFi-Backup.py
+#   Authors:	Hackoo & Seinu
+#   Purpose:	Backup WiFi Keys from MacOS, Windows and Linux 
+#   History:
 #       01/26/2015 - v1.0	Initial creation of the script in vbscript by Seinu
-#		02/05/2015 - v1.1	Hackoo shared his version of the same script
-#		02/xx/2015 - v1.2	Both scripts merged
-#		10/01/2016 - v1.3a	v1.2 rewrite doto losing script
-#		10/05/2016 - v1.3b	changed admin code added Option Explicit
-#		10/05/2016 - v1.3c	updated to camelcase
+#       02/05/2015 - v1.1	Hackoo shared his version of the same script
+#       02/xx/2015 - v1.2	Both scripts merged
+#       10/01/2016 - v1.3a	v1.2 rewrite doto losing script
+#       10/05/2016 - v1.3b	changed admin code added Option Explicit
+#       10/05/2016 - v1.3c	updated to camelcase
 #       11/20/2018 - v2.0a  updated script to python with multiplatform support
 #                               for both Windows and MacOS
 #       11/20/2018 - v2.0b  added Linux support
