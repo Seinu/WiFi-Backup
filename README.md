e1f7213c6cbf9b408ba362b0e29a141e1863f40c
Zen <zen@Zens-MacBook-Pro.local>
2019-05-07 23:16:04 -0400

    v2.1 MacOS bugfixes and Windows and Linux Code Updates

--- README.md
+++ README.md
@@ -3,16 +3,23 @@ Cross Platform python script for backing up saved WiFi Keys on MacOS, Windows an
 
 Runs out of box on both Linux and MacOS. Requires either python 2 or python 3 on Windows to run it.
 
-	File Name:	getWifiKeys.vbs, WiFi-Backup.py
-	Authors:	Hackoo & Seinu
-	Purpose:	Backup WiFi Keys from MacOS, Windows and Linux
-	History:
-		01/26/2015 - v1.0	Initial creation of the script in vbscript by Seinu
-		02/05/2015 - v1.1	Hackoo shared his version of the same script
-		02/xx/2015 - v1.2	Both scripts merged
-		10/01/2016 - v1.3a	v1.2 rewrite doto losing script
-		10/05/2016 - v1.3b	changed admin code added Option Explicit
-		10/05/2016 - v1.3c	updated to camelcase
-    	11/20/2018 - v2.0a	completely rewrote script in python with multiplatform support
-						for both Windows and MacOS
-    	11/20/2018 - v2.0b	Added Linux support via NetworkManager
+File Name:	getWifiKeys.vbs, WiFi-Backup.py
+Authors:	Hackoo & Seinu
+Purpose:	Backup WiFi Keys from MacOS, Windows and Linux
+History:
+	01/26/2015 - v1.0	Initial creation of the script in vbscript by Seinu
+	02/05/2015 - v1.1	Hackoo shared his version of the same script
+	02/xx/2015 - v1.2	Both scripts merged
+	10/01/2016 - v1.3a	v1.2 rewrite doto losing script
+	10/05/2016 - v1.3b	changed admin code added Option Explicit
+	10/05/2016 - v1.3c	updated to camelcase
+	11/20/2018 - v2.0a	completely rewrote script in python with multiplatform support
+		for both Windows and MacOS
+	11/20/2018 - v2.0b	Added Linux support via NetworkManager
+	05/07/2019 - v2.1  Fixed errors after testing on my high Sierra Hackintosh,
+		Original testing was on a Sierra Virtual Machine
+		added a check for the package networkmanager
+		changed some windows code
+		
+	TODO: Test on Proper Mac
+	TODO: Linux Update with wider distro and package support
--- WiFi-Backup.py
+++ WiFi-Backup.py
@@ -1,4 +1,4 @@
-#*****************************************************************************'
+# *****************************************************************************'
 #
 #	This program is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
@@ -13,7 +13,7 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#*****************************************************************************'
+# *****************************************************************************'
 #   File Name:	getWifiKeys.vbs, WiFi-Backup.py
 #   Authors:	Hackoo & Seinu
 #   Purpose:	Backup WiFi Keys from MacOS, Windows and Linux
@@ -24,12 +24,23 @@
 #       10/01/2016 - v1.3a  v1.2 rewrite doto losing script
 #       10/05/2016 - v1.3b  changed admin code added Option Explicit
 #       10/05/2016 - v1.3c  updated to camelcase
-#       11/20/2018 - v2.0a  completely rewrote script in python with multiplatform support
-#                               for both Windows and MacOS
+#       11/20/2018 - v2.0a  completely rewrote script in python with multiplatform
+#                               support for both Windows and MacOS
 #       11/20/2018 - v2.0b  Added Linux support via NetworkManager
+#       05/07/2019 - v2.1  Fixed errors after testing on my high Sierra Hackintosh,
+#                               Original testing was on a Sierra Virtual Machine
+#                               added a check for the package networkmanager
+#                               changed some windows code
+#                               TODO: Test on Proper Mac
+#       TODO: Linux Update with wider distro and package support
 #
-#*****************************************************************************'
-import subprocess, platform, re, ctypes, sys
+# *****************************************************************************'
+import subprocess
+import platform
+import re
+import ctypes
+import sys
+
 
 def is_admin():
     try:
@@ -37,38 +48,56 @@ def is_admin():
     except:
         return False
 
+
 if platform.system() == 'Darwin':
     objFile = open("./WiFi_List.txt", "a+")
-    SSID_List = subprocess.check_output("defaults read /Library/Preferences/SystemConfiguration/com.apple.airport.preferences | grep SSIDString", shell=True)
-    SSID = re.findall(r'\"(.+?)\"',SSID_List)
-    for x in range(0, len(SSID) - 1):
-        objFile.write(SSID[x] + ":\n\t")
-        password = subprocess.check_output("security find-generic-password -wga \"" + SSID[x] + "\"", shell=True)
-        objFile.write(password + "")
-        objFile.write("******************************************************************\n")
-        objFile.close()
+    SSID_List = subprocess.check_output(
+        "defaults read /Library/Preferences/SystemConfiguration/com.apple.airport.preferences | grep SSIDString", shell=True).decode("utf-8")
+    SSIDs = re.findall(r'= (.+?);', SSID_List)
+    for SSID in SSIDs:
+        SSIDString = re.sub('"', '', SSID)
+        objFile.write(SSIDString + ":\n\t")
+        try:
+            password = subprocess.check_output(
+                "sudo security find-generic-password -wga \"" + SSIDString + "\"", shell=True).decode("utf-8")
+        except:
+            password = "No Password"
+        objFile.write(password + "\n")
+        objFile.write(
+            "******************************************************************\n")
+
+    objFile.close()
 
 elif platform.system() == 'Windows':
     if is_admin():
-        SSID_List = subprocess.check_output("netsh wlan show all > .\\WiFi.txt", shell=True)
-        objFile = open(".\\WiFi.txt", "r")
-        strContent = objFile.read()
-        objFile.close()
-        SSID = re.findall(r'(?<=SSID name              : )\".+\"', strContent)
+        SSID_List = subprocess.check_output(
+            "netsh wlan show all", shell=True).decode("utf-8")
+        SSIDs = re.findall(r'(?<=SSID name              : )\".+\"', SSID_List)
         objFile = open(".\\WiFi_List.txt", "a+")
-        for x in range(0, len(SSID) - 1):
-            objFile.write(SSID[x] + ":\n\t")
-            password = subprocess.check_output("netsh wlan show profiles name="  + SSID[x] + " key=clear | Findstr /i ""Conten""", shell=True)
-            objFile.write(password.decode("utf-8") + "\n")
-            objFile.write("******************************************************************\n")
+        for SSID in SSIDs:
+            objFile.write(SSID + ":\n\t")
+            password = subprocess.check_output(
+                "netsh wlan show profiles name=" + SSID + " key=clear | Findstr /i ""Conten""", shell=True).decode("utf-8")
+            objFile.write(password + "\n")
+            objFile.write(
+                "******************************************************************\n")
         objFile.close()
     else:
-        if sys.version_info<(3,0,0):
-            ctypes.windll.shell32.ShellExecuteW(None, u"runas", unicode(sys.executable), unicode(__file__), None, 1)
-        elif sys.version_info>(3,0,0):
-            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+        if sys.version_info < (3, 0, 0):
+            ctypes.windll.shell32.ShellExecuteW(None, u"runas", unicode(
+                sys.executable), unicode(__file__), None, 1)
+        elif sys.version_info > (3, 0, 0):
+            ctypes.windll.shell32.ShellExecuteW(
+                None, "runas", sys.executable, __file__, None, 1)
 
 elif platform.system() == 'Linux':
-    WiFi_List = subprocess.check_output("sudo grep psk= /etc/NetworkManager/system-connections/*", shell=True)
+    retval = subprocess.call(["which", "network-manager"])
+    if retval != 0:
+        retval = subprocess.call(["which", "networkmanager"])
+        if retval != 0:
+            print("Network Manager not installed!")
+            exit()
+    WiFi_List = subprocess.check_output(
+        "sudo grep psk= /etc/NetworkManager/system-connections/*", shell=True)
     objFile = open("./WiFi_List.txt", "a+")
     objFile.write(WiFi_List)
